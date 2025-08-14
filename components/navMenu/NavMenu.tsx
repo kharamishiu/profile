eda9f87b3810469201baefcec1c04a4b7d1d869f
kharamishiu <revendelion@gmail.com>
2025-08-14 17:54:58 -0400

    end first stagev1

--- components/navMenu/NavMenu.tsx
+++ components/navMenu/NavMenu.tsx
@@ -7,7 +7,7 @@ export default function NavMenu() {
 
 
     return (
-        <nav className={`fixed top-0 left-0 right-0 z-30 transition-all duration-500 bg-amber-300`}>
+        <nav className={`fixed top-0 left-0 right-0 z-30 transition-all duration-500 bg-purple-400`}>
             <div className="max-w-7xl mx-auto px-2 sm:px-3 lg:px-6 bg-purple-400">
                 <div className="flex items-center justify-between h-16">
                     <div className="shrink-0">
